25983ea4057a9b1221ef3f611f9df4855ad3a185
tsvetinaangelova12 <94463639+tsvetinaangelova12@users.noreply.github.com>
2024-10-12 19:30:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+$(function() {
+    $('#section10').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ 
+            scrollTop: $('#section10').offset().top 
+        }, 500, 'linear');
+    });
+});
+
+function updateTimer() {
+    future  = Date.parse("June 21, 2025 15:00:00");
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("timer")
+      .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+  
+
